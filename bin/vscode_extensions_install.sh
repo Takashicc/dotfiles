52302a018c6b35b27628d21287cbdb5375ab0d53
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-03-16 10:09:55 +0900

    feat: Add some vscode extensions

--- bin/vscode_extensions_install.sh
+++ bin/vscode_extensions_install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+code --install-extension 766b.go-outliner
+code --install-extension Boto3typed.boto3-ide
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension EditorConfig.EditorConfig
 code --install-extension Equinusocio.vsc-community-material-theme
 code --install-extension Equinusocio.vsc-material-theme
+code --install-extension Gruntfuggly.todo-tree
 code --install-extension IronGeek.vscode-env
 code --install-extension JScearcy.rust-doc-viewer
 code --install-extension Shopify.ruby-lsp
@@ -12,6 +15,7 @@ code --install-extension arcanis.vscode-zipfs
 code --install-extension castwide.solargraph
 code --install-extension donjayamanne.githistory
 code --install-extension dustypomerleau.rust-syntax
+code --install-extension eamodio.gitlens
 code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-close-tag
@@ -19,13 +23,16 @@ code --install-extension formulahendry.auto-rename-tag
 code --install-extension gamunu.vscode-yarn
 code --install-extension golang.go
 code --install-extension groksrc.ruby
+code --install-extension hashicorp.terraform
 code --install-extension kaiwood.endwise
+code --install-extension kddejong.vscode-cfn-lint
 code --install-extension lixquid.calculator
 code --install-extension masakit.zenkaku-hankaku
 code --install-extension mechatroner.rainbow-csv
 code --install-extension mhutchie.git-graph
 code --install-extension mosapride.zenkaku
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.csharp
 code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
@@ -45,8 +52,10 @@ code --install-extension ms-vscode.cpptools-extension-pack
 code --install-extension ms-vscode.cpptools-themes
 code --install-extension ms-vscode.makefile-tools
 code --install-extension ms-vscode.remote-explorer
+code --install-extension ms-vscode.remote-server
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension msyrus.go-doc
+code --install-extension neptunedesign.vs-sequential-number
 code --install-extension njpwerner.autodocstring
 code --install-extension oderwat.indent-rainbow
 code --install-extension premparihar.gotestexplorer
@@ -62,5 +71,6 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension tamasfe.even-better-toml
 code --install-extension tauri-apps.tauri-vscode
 code --install-extension twxs.cmake
+code --install-extension wayou.vscode-todo-highlight
 code --install-extension wingrunr21.vscode-ruby
 code --install-extension yzhang.markdown-all-in-one
